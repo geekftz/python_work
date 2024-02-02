2921c97a8e5c61d2b62d0bf73adb6c32081f007b
fangxiang <fangxiang@immotors.com>
2024-02-02 18:31:00 +0800

    feat: add

--- making_pizza.py
+++ making_pizza.py
@@ -1,3 +1,18 @@
-import pizza
+# from pizza import *
 
-pizza.make_pizza(16, "pepperoni")
+# import pizza
+
+# pizza.make_pizza(16, "pepperoni")
+
+# from pizza import make_pizza
+
+# import pizza as p
+# from pizza import make_pizza as mp
+
+
+# mp(16, "pepperoni")
+# p.make_pizza(13, "hamburger")
+
+# from pizza import *
+
+# make_pizza(16, "pepperoni")
